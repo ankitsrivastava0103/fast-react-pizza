04323523d25b64f5ddfce56d2fcf1d45b766fee3
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-02-13 20:34:59 +0530

    Styling Menu & Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -29,12 +30,19 @@ function Cart() {
   const cart = fakeCart;
   console.log(cart);
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton>&larr; Back to menu</LinkButton>
-      <h2>Your cart, %NAME%</h2>
-      <div>
-        <Button to="/order/new">Order Pizzas</Button>
-        <button>Clear cart</button>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+      <div className="mt-6 space-x-2">
+        <Button type="primary" to="/order/new">
+          Order Pizzas
+        </Button>
+        <Button type="secondary">Clear Cart</Button>
       </div>
     </div>
   );
