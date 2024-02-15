77f1f472ee2b8eb1242aa3a4a0e4120785c6f2e8
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-02-15 10:26:34 +0530

    Redux For User State

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 const UserName = () => {
-  return <div className="hidden text-sm font-semibold md:block">Ankit</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) {
+    return null;
+  }
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 };
 
 export default UserName;
